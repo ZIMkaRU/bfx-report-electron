9b0c80431277409ad315b9c3055dd71a222b4890
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-02-24 15:56:55 +0200

    Rework auto-update release extractor

--- src/archiver/extract-with-origin-fs.js
+++ src/archiver/extract-with-origin-fs.js
@@ -1,9 +1,5 @@
 'use strict'
 
-/*
- * TODO:
- *   - need to fix zip end detection
- */
 const path = require('node:path')
 const { createWriteStream } = require('node:original-fs')
 const {
@@ -20,6 +16,9 @@ const getStreamPromise = import('get-stream')
 const openZip = promisify(yauzl.fromBuffer)
 
 class Extractor {
+  #isCanceled = false
+  #zipfile
+
   constructor (zipPath, opts) {
     this.zipPath = zipPath
     this.opts = opts ?? {}
@@ -27,62 +26,72 @@ class Extractor {
 
   async extract () {
     const buffer = await readFile(this.zipPath)
-    this.zipfile = await openZip(buffer, { lazyEntries: true })
-    this.canceled = false
+    this.#zipfile = await openZip(buffer, { lazyEntries: true })
+    this.#isCanceled = false
 
     return new Promise((resolve, reject) => {
-      this.zipfile.on('error', err => {
-        this.canceled = true
+      this.#zipfile.on('error', err => {
+        this.#isCanceled = true
+
         reject(err)
       })
-      this.zipfile.readEntry()
 
-      this.zipfile.on('close', () => {
-        if (!this.canceled) {
+      this.#zipfile.readEntry()
+
+      this.#zipfile.on('end', () => {
+        if (!this.#isCanceled) {
           resolve()
         }
       })
 
-      this.zipfile.on('entry', async entry => {
-        if (this.canceled) {
+      this.#zipfile.on('entry', async entry => {
+        if (this.#isCanceled) {
           return
         }
 
         if (entry.fileName.startsWith('__MACOSX/')) {
-          this.zipfile.readEntry()
+          this.#zipfile.readEntry()
+
           return
         }
 
-        const destDir = path.dirname(path.join(this.opts.dir, entry.fileName))
+        const destDir = path.dirname(path.join(
+          this.opts.dir,
+          entry.fileName
+        ))
 
         try {
           await mkdir(destDir, { recursive: true })
 
           const canonicalDestDir = await realpath(destDir)
-          const relativeDestDir = path.relative(this.opts.dir, canonicalDestDir)
+          const relativeDestDir = path.relative(
+            this.opts.dir,
+            canonicalDestDir
+          )
 
           if (relativeDestDir.split(path.sep).includes('..')) {
             throw new Error(`Out of bound path "${canonicalDestDir}" found while processing file ${entry.fileName}`)
           }
 
-          await this.extractEntry(entry)
-          this.zipfile.readEntry()
+          await this.#extractEntry(entry)
+          this.#zipfile.readEntry()
         } catch (err) {
-          this.canceled = true
-          this.zipfile.close()
+          this.#isCanceled = true
+          this.#zipfile.close()
+
           reject(err)
         }
       })
     })
   }
 
-  async extractEntry (entry) {
-    if (this.canceled) {
+  async #extractEntry (entry) {
+    if (this.#isCanceled) {
       return
     }
 
-    if (this.opts.onEntry) {
-      this.opts.onEntry(entry, this.zipfile)
+    if (typeof this.opts.onEntry === 'function') {
+      this.opts.onEntry(entry, this.#zipfile)
     }
 
     const dest = path.join(this.opts.dir, entry.fileName)
@@ -97,39 +106,53 @@ class Extractor {
     let isDir = (mode & IFMT) === IFDIR
 
     // Failsafe, borrowed from jsZip
-    if (!isDir && entry.fileName.endsWith('/')) {
+    if (
+      !isDir &&
+      entry.fileName.endsWith('/')
+    ) {
       isDir = true
     }
 
     // check for windows weird way of specifying a directory
     // https://github.com/maxogden/extract-zip/issues/13#issuecomment-154494566
     const madeBy = entry.versionMadeBy >> 8
-    if (!isDir) isDir = (madeBy === 0 && entry.externalFileAttributes === 16)
+    if (!isDir) {
+      isDir = (
+        madeBy === 0 &&
+        entry.externalFileAttributes === 16
+      )
+    }
 
-    const procMode = this.getExtractedMode({ mode, isDir }) & 0o777
+    const procMode = this.#getExtractedMode({ mode, isDir }) & 0o777
 
     // always ensure folders are created
     const destDir = isDir ? dest : path.dirname(dest)
-
-    const mkdirOptions = { recursive: true }
-    if (isDir) {
-      mkdirOptions.mode = procMode
+    const mkdirOptions = {
+      recursive: true,
+      ...(isDir ? { mode: procMode } : {})
     }
-    await mkdir(destDir, mkdirOptions)
-    if (isDir) return
 
-    const readStream = await promisify(this.zipfile.openReadStream.bind(this.zipfile))(entry)
+    await mkdir(destDir, mkdirOptions)
+
+    if (isDir) {
+      return
+    }
+
+    const readStream = await promisify(this.#zipfile.openReadStream
+      .bind(this.#zipfile))(entry)
 
     if (symlink) {
       const { default: getStream } = await getStreamPromise
       const link = await getStream(readStream)
       await fsSymlink(link, dest)
-    } else {
-      await pipeline(readStream, createWriteStream(dest, { mode: procMode }))
+
+      return
     }
+
+    await pipeline(readStream, createWriteStream(dest, { mode: procMode }))
   }
 
-  getExtractedMode (args) {
+  #getExtractedMode (args) {
     const { mode: entryMode, isDir } = args ?? {}
 
     if (entryMode !== 0) {
