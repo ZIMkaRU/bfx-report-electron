8090b2cf1c11e3fb1ca5ecc7a1757b376b6f2ec6
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-03-23 13:50:14 +0200

    Add vi translation for NSDownloadsFolderUsageDescription on mac

--- electron-builder-config.js
+++ electron-builder-config.js
@@ -180,7 +180,7 @@ module.exports = {
     entitlementsInherit: 'build/entitlements.mac.inherit.plist',
     extendInfo: {
       NSDownloadsFolderUsageDescription: 'This app saves downloaded reports directly to your Downloads folder for your convenience',
-      CFBundleLocalizations: ['en', 'es', 'pt_BR', 'ru']
+      CFBundleLocalizations: ['en', 'es', 'pt_BR', 'ru', 'vi']
     },
     extraResources: [
       {
