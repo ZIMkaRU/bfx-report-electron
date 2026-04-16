0d0af4af8cd0185d951c59e82063229f099a62da
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-16 15:42:21 +0300

    Add tester: should error modal win be suppressed

--- src/error-manager/log-exclusions/index.js
+++ src/error-manager/log-exclusions/index.js
@@ -7,9 +7,20 @@ const {
   isCodeSignatureError,
   isBfxApiServerAvailabilityError,
   isDatabaseLockError,
-  isNetworkTimeoutError
+  isNetworkTimeoutError,
+  isDiffUpdateDownloadError,
+  isGHUpdateDownloadError,
+  isHtmlLoadingForPdfError,
+  isPdfGenerationError,
+  isDeprecationWarningFsStatsError,
+  isDeprecationWarningUrlParseError
 } = errorTesters
 
+const { isENetError } = require(
+  '../../../bfx-reports-framework/workers/loc.api/helpers/api-errors-testers'
+)
+
+// If log is skipped, a new GH issue will be suppressed
 const shouldLogBeSkipped = (err) => (
   isContextIsolationError(err) ||
   isInetDisconnectionError(err) ||
@@ -19,7 +30,19 @@ const shouldLogBeSkipped = (err) => (
   isNetworkTimeoutError(err)
 )
 
+// Don't open a new GH issue, but log anyway
+const shouldErrorModalWinBeSuppressed = (err) => (
+  isENetError(err) ||
+  isDiffUpdateDownloadError(err) ||
+  isGHUpdateDownloadError(err) ||
+  isHtmlLoadingForPdfError(err) ||
+  isPdfGenerationError(err) ||
+  isDeprecationWarningFsStatsError(err) ||
+  isDeprecationWarningUrlParseError(err)
+)
+
 module.exports = {
   errorTesters,
-  shouldLogBeSkipped
+  shouldLogBeSkipped,
+  shouldErrorModalWinBeSuppressed
 }
