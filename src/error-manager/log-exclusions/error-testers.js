9e37c8fddcba27a34f861f50b56b8cb5487506d8
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-16 14:23:36 +0300

    Add error testers to skip logging

--- src/error-manager/log-exclusions/error-testers.js
+++ src/error-manager/log-exclusions/error-testers.js
@@ -12,10 +12,44 @@ const _getErrorString = (err) => {
   return errStr
 }
 
+const _testRegExp = (regExp, err) => (
+  regExp.test(_getErrorString(err))
+)
+
 const isDocumentsPathGettingError = (err) => (
-  /Failed to get 'documents' path/i.test(_getErrorString(err))
+  _testRegExp(err, /Failed to get 'documents' path/i)
+)
+
+const isContextIsolationError = (err) => (
+  _testRegExp(err, /contextIsolation is deprecated/i)
+)
+
+const isInetDisconnectionError = (err) => (
+  _testRegExp(err, /ERR_INTERNET_DISCONNECTED/i)
+)
+
+const isCodeSignatureError = (err) => (
+  _testRegExp(err, /Could not get code signature/i)
+)
+
+const isBfxApiServerAvailabilityError = (err) => (
+  _testRegExp(err, /ERR_BFX_API_SERVER_IS_NOT_AVAILABLE/i)
+)
+
+const isDatabaseLockError = (err) => (
+  _testRegExp(err, /database is locked/i)
+)
+
+const isNetworkTimeoutError = (err) => (
+  _testRegExp(err, /network timeout/i)
 )
 
 module.exports = {
-  isDocumentsPathGettingError
+  isDocumentsPathGettingError,
+  isContextIsolationError,
+  isInetDisconnectionError,
+  isCodeSignatureError,
+  isBfxApiServerAvailabilityError,
+  isDatabaseLockError,
+  isNetworkTimeoutError
 }
